8a69ca3d31f6ff371bace37889ca4251110ffc42
Rick Dillon <Rick_Dillon@bose.com>
2017-08-25 16:55:06 -0400

    added test dependencies mocha and chai, babel plugin and .babelrc for es2015 support in mocha tests

---
+++ tests/components/verbs/index.js
@@ -0,0 +1,22 @@
+//** Verbs test file */
+import Verbs from '../../../src/components/verbs/Verbs'
+
+var assert = require('chai').assert;
+
+
+
+describe('Verb components test code ...', function(){
+    let verbs = null;
+
+    before(function(){
+        verbs = new Verbs();
+    })
+    it('verbs is an object', function(){
+        assert.exists(verbs, 'verbs is either `null` or `undefined`');
+    })
+
+    it('should validate verb names', function() {
+        assert.isTrue(verbs.validateVerbName("parlare"), 'parlare is a valid verb name')
+        assert.isFalse(verbs.validateVerbName("parlaro"), 'parlare is a valid verb name')
+    });
+})
--- tests/index.js
+++
@@ -1,7 +0,0 @@
-var assert = require('assert');
-
-describe('empty test for astrters', function(){
-    it('empty test should return success', function() {
-        assert('A', 'A')
-    })
-})
