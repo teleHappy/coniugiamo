25ff218e1f8b6e14139bc47181a641eafc5713e3
Rick Dillon <rdillon.enservio@gmal.com>
2017-08-27 14:03:12 -0400

    more clean up; added tests for present tense and future tense regular verbs

--- src/components/verbs/areTableGenerator.js
+++ src/components/verbs/areTableGenerator.js
@@ -19,41 +19,43 @@ class areTableGenerator {
     }
     
     getVerbStem(name, tense, pronoun){
-        let stem = '';
+        
+        let stem = name.slice(0, name.length-3);
         
         if(tense === 'present'){
-           return this.getPresentVerbStem(name, pronoun)
+           stem = this.getPresentVerbStem(name, pronoun)
         }
 
         if(tense === 'future'){
-            stem = name.slice(0, name.length-1)
+            stem = this.getFutureVerbStem(name);
         }
-        else{
-            stem = name.slice(0, name.length-3)
-        }
+
         return stem
     }
 
     getPresentVerbStem(name, pronoun){
     
-    let stem = name.slice(0, name.length-3);
-    
-    //handle -ciare, -giare endings
-    if(name.slice(name.length-5) === 'ciare' || name.slice(name.length-5) === 'giare'){
-        //drop the -are and retain the i of the ending
-        //do not write a double 'i' in the present indicative endings for 3rd person singular or 1st person plural
-        stem = (pronoun === 'lui/lei/Lei' || pronoun === 'noi') ? name.slice(0, name.length-4) : stem
+        let stem = name.slice(0, name.length-3);
+        
+        //handle -ciare, -giare endings
+        if(name.slice(name.length-5) === 'ciare' || name.slice(name.length-5) === 'giare'){
+            //drop the -are and retain the i of the ending
+            //do not write a double 'i' in the present indicative endings for 3rd person singular or 1st person plural
+            stem = (pronoun === 'lui/lei/Lei' || pronoun === 'noi') ? name.slice(0, name.length-4) : stem;
+        }
+
+        //handle -care, -gare endings
+        if(name.slice(name.length-4) === 'care' || name.slice(name.length-4) === 'gare'){
+            //drop the -are but add an 'h' before the present indicative endings 'i' and 'iamo'
+            stem = (pronoun === 'lui/lei/Lei' || pronoun === 'noi') ? stem + 'h' : stem;
+        }
+
+        return stem;
     }
 
-    //handle -care, -gare endings
-    if(name.slice(name.length-4) === 'care' || name.slice(name.length-4) === 'gare'){
-        //drop the -are but add an 'h' before the present indicative endings 'i' and 'iamo'
-        stem = (pronoun === 'lui/lei/Lei' || pronoun === 'noi') ? stem + 'h' : stem
+    getFutureVerbStem(name){
+        return name.slice(0, name.length-3) + "er"
     }
-
-    return stem
-    }
-
 }
 
 export default areTableGenerator
--- tests/components/verbs/index.js
+++ tests/components/verbs/index.js
@@ -6,7 +6,7 @@ var assert = require('chai').assert;
 describe('Testing Verb component', function(){
     let verb = null;
 
-    beforeEach(function(){
+    before(function(){
         verb = new Verb();
     })
     it('verb is neither null or undefined', function(){
@@ -18,45 +18,56 @@ describe('Testing Verb component', function(){
         assert.isFalse(verb.validateVerbName("parlar"), 'parlare is a valid verb name')
     });
 
-    it('returns properly formatted regular verb stem for standard "are" verbs', function(){
-        assert.equal(verb.areTableGenerator.getVerbStem('parlare', 'present', 'io'), 'parl')
-    })
-
-    it('returns properly formatted regular verb stem for "-ciare", "-giare" verbs', function(){
-        assert.equal(verb.areTableGenerator.getVerbStem('cominciare', 'present', 'io'), 'cominci')
-        assert.equal(verb.areTableGenerator.getVerbStem('cominciare', 'present', 'tu'), 'cominci')
-        assert.equal(verb.areTableGenerator.getVerbStem('cominciare', 'present', 'lui/lei/Lei'), 'cominc')
-        assert.equal(verb.areTableGenerator.getVerbStem('cominciare', 'present', 'noi'), 'cominc')
-        assert.equal(verb.areTableGenerator.getVerbStem('cominciare', 'present', 'voi'), 'cominci')
-        assert.equal(verb.areTableGenerator.getVerbStem('cominciare', 'present', 'loro/Loro'), 'cominci')
-        
-        assert.equal(verb.areTableGenerator.getVerbStem('mangiare', 'present', 'io'), 'mangi')
-        assert.equal(verb.areTableGenerator.getVerbStem('mangiare', 'present', 'tu'), 'mangi')
-        assert.equal(verb.areTableGenerator.getVerbStem('mangiare', 'present', 'lui/lei/Lei'), 'mang')
-        assert.equal(verb.areTableGenerator.getVerbStem('mangiare', 'present', 'noi'), 'mang')
-        assert.equal(verb.areTableGenerator.getVerbStem('mangiare', 'present', 'voi'), 'mangi')
-        assert.equal(verb.areTableGenerator.getVerbStem('mangiare', 'present', 'loro/Loro'), 'mangi')
-    })
-
-    it('returns properly formatted regular verb stem for "-care", "-gare" verbs', function(){
-        assert.equal(verb.areTableGenerator.getVerbStem('cercare', 'present', 'io'), 'cerc')
-        assert.equal(verb.areTableGenerator.getVerbStem('cercare', 'present', 'tu'), 'cerc')
-        assert.equal(verb.areTableGenerator.getVerbStem('cercare', 'present', 'lui/lei/Lei'), 'cerch')
-        assert.equal(verb.areTableGenerator.getVerbStem('cercare', 'present', 'noi'), 'cerch')
-        assert.equal(verb.areTableGenerator.getVerbStem('cercare', 'present', 'io'), 'cerc')
-        assert.equal(verb.areTableGenerator.getVerbStem('cercare', 'present', 'io'), 'cerc')
-  
-        assert.equal(verb.areTableGenerator.getVerbStem('pagare', 'present', 'io'), 'pag')
-        assert.equal(verb.areTableGenerator.getVerbStem('pagare', 'present', 'tu'), 'pag')
-        assert.equal(verb.areTableGenerator.getVerbStem('pagare', 'present', 'lui/lei/Lei'), 'pagh')
-        assert.equal(verb.areTableGenerator.getVerbStem('pagare', 'present', 'noi'), 'pagh')
-        assert.equal(verb.areTableGenerator.getVerbStem('pagare', 'present', 'voi'), 'pag')
-        assert.equal(verb.areTableGenerator.getVerbStem('pagare', 'present', 'loro/Loro'), 'pag')
-    })
-
-    it('returns properly formatted regular verb stem for standard "ere" verbs')
+    describe('Testing regular present tense verb stems for conjugation', function(){
+        it('present tense: returns properly formatted regular verb stem for standard "are" verbs', function(){
+            assert.equal(verb.areTableGenerator.getVerbStem('parlare', 'present', 'io'), 'parl')
+        })
     
-    it('returns properly formatted regular verb stem for type 1 "ire" verbs')
-    it('returns properly formatted regular verb stem for type 2 "ire" verbs')
+        it('present tense:returns properly formatted regular verb stem for "-ciare", "-giare" verbs', function(){
+            assert.equal(verb.areTableGenerator.getVerbStem('cominciare', 'present', 'io'), 'cominci')
+            assert.equal(verb.areTableGenerator.getVerbStem('cominciare', 'present', 'tu'), 'cominci')
+            assert.equal(verb.areTableGenerator.getVerbStem('cominciare', 'present', 'lui/lei/Lei'), 'cominc')
+            assert.equal(verb.areTableGenerator.getVerbStem('cominciare', 'present', 'noi'), 'cominc')
+            assert.equal(verb.areTableGenerator.getVerbStem('cominciare', 'present', 'voi'), 'cominci')
+            assert.equal(verb.areTableGenerator.getVerbStem('cominciare', 'present', 'loro/Loro'), 'cominci')
+            
+            assert.equal(verb.areTableGenerator.getVerbStem('mangiare', 'present', 'io'), 'mangi')
+            assert.equal(verb.areTableGenerator.getVerbStem('mangiare', 'present', 'tu'), 'mangi')
+            assert.equal(verb.areTableGenerator.getVerbStem('mangiare', 'present', 'lui/lei/Lei'), 'mang')
+            assert.equal(verb.areTableGenerator.getVerbStem('mangiare', 'present', 'noi'), 'mang')
+            assert.equal(verb.areTableGenerator.getVerbStem('mangiare', 'present', 'voi'), 'mangi')
+            assert.equal(verb.areTableGenerator.getVerbStem('mangiare', 'present', 'loro/Loro'), 'mangi')
+        })
+    
+        it('present tense: returns properly formatted regular verb stem for "-care", "-gare" verbs', function(){
+            assert.equal(verb.areTableGenerator.getVerbStem('cercare', 'present', 'io'), 'cerc')
+            assert.equal(verb.areTableGenerator.getVerbStem('cercare', 'present', 'tu'), 'cerc')
+            assert.equal(verb.areTableGenerator.getVerbStem('cercare', 'present', 'lui/lei/Lei'), 'cerch')
+            assert.equal(verb.areTableGenerator.getVerbStem('cercare', 'present', 'noi'), 'cerch')
+            assert.equal(verb.areTableGenerator.getVerbStem('cercare', 'present', 'io'), 'cerc')
+            assert.equal(verb.areTableGenerator.getVerbStem('cercare', 'present', 'io'), 'cerc')
+      
+            assert.equal(verb.areTableGenerator.getVerbStem('pagare', 'present', 'io'), 'pag')
+            assert.equal(verb.areTableGenerator.getVerbStem('pagare', 'present', 'tu'), 'pag')
+            assert.equal(verb.areTableGenerator.getVerbStem('pagare', 'present', 'lui/lei/Lei'), 'pagh')
+            assert.equal(verb.areTableGenerator.getVerbStem('pagare', 'present', 'noi'), 'pagh')
+            assert.equal(verb.areTableGenerator.getVerbStem('pagare', 'present', 'voi'), 'pag')
+            assert.equal(verb.areTableGenerator.getVerbStem('pagare', 'present', 'loro/Loro'), 'pag')
+        })
+    
+        it('present tense: returns properly formatted regular verb stem for standard "ere" verbs')
+        
+        it('present tense: returns properly formatted regular verb stem for type 1 "ire" verbs')
+        it('present tense: returns properly formatted regular verb stem for type 2 "ire" verbs')
+    })
+    
+    describe('Testing regular future tense verb stems for conjugation', function(){
+        it('future tense: returns properly formatted regular verb stem for "are verbs', function(){
+            assert.equal(verb.areTableGenerator.getVerbStem('parlare', 'future', 'io'), 'parler')
+        })
+    })
+    
+
+
 
 })
