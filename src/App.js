fbbe0c9dfb5c549234f58d4a0a94a88a1e68c2e8
teleHappy <rdillon.enservio@gmail.com>
2017-09-04 17:10:28 -0400

    Typo in App header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <h3 style={{"backgroundColor": "#222", "height": "auto", "padding": "20px", "color": "white"}}>Italian Verq Quiz</h3>
+          <h3 style={{"backgroundColor": "#222", "height": "auto", "padding": "20px", "color": "white"}}>Italian Verb Quiz</h3>
           <Quiz />
       </div>
     );
