03f66c7b7c56e2e4c0b374f52a8eca8c54e85e57
teleHappy <rdillon.enservio@gmail.com>
2017-09-04 23:48:36 -0400

    basic style improvements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import Quiz from './components/quiz/Quiz'
-
+import './App.css'
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <h3 style={{"backgroundColor": "#222", "height": "auto", "padding": "20px", "color": "white"}}>Italian Verb Quiz</h3>
+          <h3 style={{"backgroundColor": "#de3941", "height": "auto", "padding": "20px", "color": "white"}}>Italian Verb Quiz</h3>
           <Quiz />
       </div>
     );
--- src/components/question/Question.js
+++ src/components/question/Question.js
@@ -16,26 +16,21 @@ class Question extends Component{
     // returns display question str
     getDisplayQuestion(){
         return (
-            <p style={{"padding": "20px"}}>Which is the correct <strong>{rules['pronouns'][this.props.params.personIndex]}</strong> form for the <strong>{this.props.params.tense}</strong> tense of <b>{this.props.params.verbName}</b>?</p>
+            <p className="questionText">Which is the correct <strong>{rules['pronouns'][this.props.params.personIndex]}</strong> form for the <strong>{this.props.params.tense}</strong> tense of <b>{this.props.params.verbName}</b>?</p>
         )
-    }
-
-    getListItem(item){
-        debugger
-        return (<li>item[1]</li>)
     }
 
     getAnswerList(){
         const verbTablesArray = this.props.params.verbTablesArray
         const personIndex = this.props.params.personIndex
         let listItems = verbTablesArray.map(function(vt, idx){
-            return (<li key={idx}>{vt[personIndex][1]}</li>)
+            return (<li key={idx} className="questionText">{vt[personIndex][1]}</li>)
         })
         
         return(
-            <ul>
+            <ol>
                 {listItems}
-            </ul>
+            </ol>
         )
 
     }
@@ -43,17 +38,16 @@ class Question extends Component{
     render(){
         if(this.props.params.verbName === "") {
             return(
-            <p>Click Next Question to get Started</p>
+            <p className="questionText">Click Next Question to get Started</p>
         )}
         return(           
             <div>
                 {this.getDisplayQuestion()}          
                 {this.getAnswerList()}
-                <VerbDisplayTable verbTable={this.props.params.verbTablesArray[0]}/>
             </div>
+            //<VerbDisplayTable verbTable={this.props.params.verbTablesArray[0]}/>
         )
         
-        //this.getCorrectTableLink()
     }
 
 }
