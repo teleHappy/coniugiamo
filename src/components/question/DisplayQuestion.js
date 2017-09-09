465d3e81a847f1ea81ce4e445a5af67d7194a571
teleHappy <rdillon.enservio@gmail.com>
2017-09-08 20:33:11 -0400

    fixed defects in 'are' verb conjugation logic, more styling, updated tests

--- src/components/question/DisplayQuestion.js
+++ src/components/question/DisplayQuestion.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const DisplayQuestion = ({pronoun, tense, verbName}) => {
     return(
-        <p className="questionText">Which is the correct <strong>{pronoun}</strong> form for the <strong>{tense}</strong> tense of <b>{verbName}</b>?</p>
+        <p className="displayQuestionText">Which is the correct <span className="param">{pronoun}</span> form for the <span className="param">{tense}</span> tense of <span className="param">{verbName}</span>?</p>
     )
 }
 
