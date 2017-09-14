132acc051a8b3487a627f98708506961eb099759
teleHappy <rdillon.enservio@gmail.com>
2017-09-14 17:54:32 -0400

    linted registerServiceWorker.js

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,3 +1,5 @@
+
+
 // In production, we register a service worker to serve assets from local cache.
 
 // This lets the app load faster on subsequent visits in production, and gives
@@ -9,14 +11,15 @@
 // This link also includes instructions on opting out of this behavior.
 
 const isLocalhost = Boolean(
-  window.location.hostname === 'localhost' ||
+    window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
     window.location.hostname === '[::1]' ||
     // 127.0.0.1/8 is considered localhost for IPv4.
     window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
 );
+
 
 export default function register () {
 
@@ -27,9 +30,9 @@ export default function register () {
 
         if (publicUrl.origin !== window.location.origin) {
 
-      // Our service worker won't work if PUBLIC_URL is on a different origin
-      // From what our page is served on. This might happen if a CDN is used to
-      // Serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
+        // Our service worker won't work if PUBLIC_URL is on a different origin
+        // From what our page is served on. This might happen if a CDN is used to
+        // Serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
             return;
 
         }
@@ -37,7 +40,7 @@ export default function register () {
         window.addEventListener('load', () => {
 
             const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-
+            
             if (!isLocalhost) {
 
         // Is not local host. Just register service worker
