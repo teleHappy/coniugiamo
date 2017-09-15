f53cd802e0d90df2a02bc20a0f5bb03f04f6f911
teleHappy <rdillon.enservio@gmail.com>
2017-09-15 10:30:13 -0400

    Simnplifying verb table display

--- src/components/verb/ui/VerbDisplayTable.js
+++ src/components/verb/ui/VerbDisplayTable.js
@@ -3,52 +3,30 @@ import React from 'react'
 // TODO: far from beautiful, this function can be improved...
 
 const VerbDisplayTable = ({verbTablesArray}) =>{
+
+        const verbTableArray = verbTablesArray[0];
         
-        const qArr = verbTablesArray[0];
-        // eslint-disable-next-line
-        let s1Label, s1Verb, s2Label, s2Verb, s3Label, s3Verb, p1Label, p1Verb, p2Label, p2Verb, p3Label, p3Verb = "";
-        if(qArr.length > 0){
-            s1Label = qArr[0][0];
-            s1Verb = qArr[0][1];
-    
-            s2Label = qArr[1][0];
-            s2Verb = qArr[1][1];
-    
-            s3Label = qArr[2][0];
-            s3Verb = qArr[2][1];
-    
-            p1Label = qArr[3][0];
-            p1Verb = qArr[3][1];
-    
-            p2Label = qArr[4][0];
-            p2Verb = qArr[4][1];
-    
-            p3Label = qArr[5][0];
-            p3Verb = qArr[5][1];
-            
-        }            
         return (
             <div id="verbTableContainer">
                 <table>
-                    
                     <tbody>
                         <tr>
-                            <th>{s1Label}</th>
-                            <td>{s1Verb}</td>
-                            <th>{p2Label}</th>
-                            <td>{p1Verb}</td>
+                            <th>{verbTableArray[0][0]}</th>
+                            <td>{verbTableArray[0][1]}</td>
+                            <th>{verbTableArray[3][0]}</th>
+                            <td>{verbTableArray[3][1]}</td>
                         </tr>
                         <tr>
-                            <th>{s2Label}</th>
-                            <td>{s2Verb}</td>
-                            <th>{p2Label}</th>
-                            <td>{p2Verb}</td>
+                            <th>{verbTableArray[1][0]}</th>
+                            <td>{verbTableArray[1][1]}</td>
+                            <th>{verbTableArray[4][0]}</th>
+                            <td>{verbTableArray[4][1]}</td>
                         </tr>
                         <tr>
-                            <th>{s3Label}</th>
-                            <td>{s3Verb}</td>
-                            <th>{p3Label}</th>
-                            <td>{p3Verb}</td>
+                            <th>{verbTableArray[2][0]}</th>
+                            <td>{verbTableArray[2][1]}</td>
+                            <th>{verbTableArray[5][0]}</th>
+                            <td>{verbTableArray[5][1]}</td>
                         </tr>
                     </tbody>
                 </table>
