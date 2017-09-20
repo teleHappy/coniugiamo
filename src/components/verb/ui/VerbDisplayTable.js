d372cd13d256927c8f56b5813c1204463d89a719
teleHappy <rdillon.enservio@gmail.com>
2017-09-19 23:23:42 -0400

    VerbTable made responsive via flex and grid

--- src/components/verb/ui/VerbDisplayTable.js
+++ src/components/verb/ui/VerbDisplayTable.js
@@ -7,41 +7,30 @@ const VerbDisplayTable = ({verbTablesArray}) =>{
         const verbTableArray = verbTablesArray[0];
 
         return (
-            <div id="verbTableContainer">
-                <div>
-                <table>
-                    <tbody>
-                        
-                        <tr>
-                            <th>{verbTableArray[0][0]}</th>
-                            <td>{verbTableArray[0][1]}</td>
-                        </tr>
-                        
-                        <tr>
-                            <th>{verbTableArray[1][0]}</th>
-                            <td>{verbTableArray[1][1]}</td>
-                        </tr>
-                        <tr>
-                            <th>{verbTableArray[2][0]}</th>
-                            <td>{verbTableArray[2][1]}</td>
-                        </tr>
-                        <tr>
-                            <th>{verbTableArray[3][0]}</th>
-                            <td>{verbTableArray[3][1]}</td>
-                        </tr>
-                        <tr>    
-                            <th>{verbTableArray[4][0]}</th>
-                            <td>{verbTableArray[4][1]}</td>
-                        </tr>
-                        
-                        <tr>                            
-                            <th>{verbTableArray[5][0]}</th>
-                            <td>{verbTableArray[5][1]}</td>
-                        </tr>
-                    </tbody>
-                </table>
-                </div>
-            </div>)
+        
+            
+            <div className="tableWrapper">
+            <div className="leftCol">
+                <div>{verbTableArray[0][0]}</div>
+                <div>{verbTableArray[0][1]}</div>
+                <div>{verbTableArray[1][0]}</div>
+                <div>{verbTableArray[1][1]}</div>
+                <div>{verbTableArray[2][0]}</div>
+                <div>{verbTableArray[2][1]}</div>
+            </div>
+            
+            <div className="rightCol">
+                <div>{verbTableArray[3][0]}</div>
+                <div>{verbTableArray[3][1]}</div>
+                <div>{verbTableArray[4][0]}</div>
+                <div>{verbTableArray[4][1]}</div>
+                <div>{verbTableArray[5][0]}</div>
+                <div>{verbTableArray[5][1]}</div>
+            </div>
+            
+        </div>
+        
+        )
     }
 
  export default VerbDisplayTable;
