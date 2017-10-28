a7ee2d207f520e6fca506cd3a6aa297e1d149d12
teleHappy <rdillon.enservio@gmail.com>
2017-10-28 10:20:35 -0400

    verb stem and ending logic for ire and ere

--- src/data/verbs/ire.js
+++ src/data/verbs/ire.js
@@ -27,6 +27,61 @@ const ire = [
         'gerund': 'endo',
         'aux_verb': 'avere'
     },
+    {
+        'name': 'prefire',
+        'type': 2,
+        'translation': 'to prefer',
+        'regular': 'regular',
+        'past_participle': 'ito',
+        'gerund': 'endo',
+        'aux_verb': 'avere'
+    },
+    {
+        'name': 'uscire',
+        'type': 1,
+        'translation': 'to exit',
+        'regular': 'regular',
+        'past_participle': 'ito',
+        'gerund': 'endo',
+        'aux_verb': 'avere'
+    },
+    {
+        'name': 'sentire',
+        'type': 1,
+        'translation': 'to feel',
+        'regular': 'regular',
+        'past_participle': 'ito',
+        'gerund': 'endo',
+        'aux_verb': 'avere'
+    },
+    {
+        'name': 'seguire',
+        'type': 1,
+        'translation': 'to follow',
+        'regular': 'regular',
+        'past_participle': 'ito',
+        'gerund': 'endo',
+        'aux_verb': 'avere'
+    },
+    {
+        'name': 'pulire',
+        'type': 2,
+        'translation': 'to clean',
+        'regular': 'regular',
+        'past_participle': 'ito',
+        'gerund': 'endo',
+        'aux_verb': 'avere'
+    },
+    {
+        'name': 'costuire',
+        'type': 2,
+        'translation': 'to build',
+        'regular': 'regular',
+        'past_participle': 'ito',
+        'gerund': 'endo',
+        'aux_verb': 'avere'
+    }
+        
 
 ];
 
