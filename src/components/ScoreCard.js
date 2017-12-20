f1bea701444d8351dfb595942d849b3cdd49f015
teleHappy <rdillon.enservio@gmail.com>
2017-12-20 09:19:39 -0500

    Adding exclamations to score card

--- src/components/ScoreCard.js
+++ src/components/ScoreCard.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React from 'react';
 
-const ScoreCard = props => (
-  <div className="scoreContainer">
-    <div className="score">
-      <h3>Molto bene!</h3>
-      <p>You scored {props.correctAnswers / props.totalQuestions * 100}%</p>
-    </div>
-  </div>
-);
+const ScoreCard = props => {
+
+    const exclamations = [
+        'Peccato',
+        'Mamma Mia',
+        'Bravo',
+        'Molt Bene',
+        'Benissimo'
+    ];
+
+    const getScore = () => props.correctAnswers / props.totalQuestions * 100
+
+    const getExclamation = () => <h3>{exclamations[props.correctAnswers - 1]}!!!</h3>
+
+    const getScoreGreeting = () => <div><p>You scored {getScore()}%</p></div>
+
+    return (
+        <div className="scoreContainer">
+            <div className="score">
+                {getExclamation()}
+                {getScoreGreeting()}
+            </div>
+        </div>
+    );
+
+};
 
 export default ScoreCard;
